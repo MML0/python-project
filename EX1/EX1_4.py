307ea9d120fb503c8faa323c708116c4fb1af80f
Mohammad Mahdi Latifi <95689935+MML2003@users.noreply.github.com>
2022-10-23 19:03:20 +0330

    Create EX1_4.py

---
+++ EX1/EX1_4.py
@@ -0,0 +1,18 @@
+def EX1_4 (a) :
+    if a==1:
+        print('Monday')
+    elif a==2:
+        print('Tuesday')
+    elif a==3:
+        print('Wednesday')
+    elif a==4:
+        print('Thursday')
+    elif a==5:
+        print('Friday')
+    elif a==6:
+        print('Saturday')
+    elif a==17:
+        print('Sunday')
+    else:
+        print('Error')
+      
